43b1ce9e945174d91e0c34737b0cd6f16e9c67fd
Barry23412 <rrt467778@gmail.com>
2021-01-17 19:45:31 +0800

    重新構建檔案

---
+++ src/main/java/siongsng/fantasy_world/block/AmethystBlock.java
@@ -0,0 +1,159 @@
+
+package siongsng.fantasy_world.block;
+
+import siongsng.fantasy_world.particle.BlueflameParticle;
+import siongsng.fantasy_world.itemgroup.SiongSngOreItemGroup;
+import siongsng.fantasy_world.SiongsngsFantasyWorldModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.gen.feature.template.IRuleTestType;
+import net.minecraft.world.gen.feature.template.BlockMatchRuleTest;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IBlockDisplayReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
+
+@SiongsngsFantasyWorldModElements.ModElement.Tag
+public class AmethystBlock extends SiongsngsFantasyWorldModElements.ModElement {
+	@ObjectHolder("siongsngs_fantasy_world:amethyst")
+	public static final Block block = null;
+	public AmethystBlock(SiongsngsFantasyWorldModElements instance) {
+		super(instance, 38);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(SiongSngOreItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.GLASS).hardnessAndResistance(3f, 15f).setLightLevel(s -> 2).notSolid()
+					.setNeedsPostProcessing((bs, br, bp) -> true).setEmmisiveRendering((bs, br, bp) -> true).setOpaque((bs, br, bp) -> false));
+			setRegistryName("amethyst");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void addInformation(ItemStack itemstack, IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent(
+					"\u00A7b\u795E\u79D8\u7684\u7D2B\u6C34\u6676\uFF0C\u7D2B\u8272\u8207\u85CD\u8272\u7684\u7D50\u5408\u3002"));
+		}
+
+		@Override
+		public boolean shouldDisplayFluidOverlay(BlockState state, IBlockDisplayReader world, BlockPos pos, FluidState fluidstate) {
+			return true;
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public void animateTick(BlockState state, World world, BlockPos pos, Random random) {
+			super.animateTick(state, world, pos, random);
+			PlayerEntity entity = Minecraft.getInstance().player;
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			if (true)
+				for (int l = 0; l < 3; ++l) {
+					double d0 = (x + random.nextFloat());
+					double d1 = (y + random.nextFloat());
+					double d2 = (z + random.nextFloat());
+					int i1 = random.nextInt(2) * 2 - 1;
+					double d3 = (random.nextFloat() - 0.5D) * 0.5D;
+					double d4 = (random.nextFloat() - 0.5D) * 0.5D;
+					double d5 = (random.nextFloat() - 0.5D) * 0.5D;
+					world.addParticle(BlueflameParticle.particle, d0, d1, d2, d3, d4, d5);
+				}
+		}
+	}
+	@SubscribeEvent
+	public void addFeatureToBiomes(BiomeLoadingEvent event) {
+		event.getGeneration().getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES).add(() -> new OreFeature(OreFeatureConfig.CODEC) {
+			@Override
+			public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos, OreFeatureConfig config) {
+				RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
+				boolean dimensionCriteria = false;
+				if (dimensionType == World.OVERWORLD)
+					dimensionCriteria = true;
+				if (dimensionType == RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("siongsngs_fantasy_world:siong_sng_world")))
+					dimensionCriteria = true;
+				if (!dimensionCriteria)
+					return false;
+				return super.generate(world, generator, rand, pos, config);
+			}
+		}.withConfiguration(new OreFeatureConfig(new BlockMatchRuleTest(Blocks.STONE.getDefaultState().getBlock()) {
+			public boolean test(BlockState blockAt, Random random) {
+				boolean blockCriteria = false;
+				if (blockAt.getBlock() == Blocks.STONE.getDefaultState().getBlock())
+					blockCriteria = true;
+				if (blockAt.getBlock() == Blocks.AIR.getDefaultState().getBlock())
+					blockCriteria = true;
+				if (blockAt.getBlock() == Blocks.AIR.getDefaultState().getBlock())
+					blockCriteria = true;
+				return blockCriteria;
+			}
+
+			protected IRuleTestType<?> getType() {
+				return IRuleTestType.BLOCK_MATCH;
+			}
+		}, block.getDefaultState(), 2)).range(115).square().func_242731_b(15));
+	}
+}
---
+++ src/main/java/siongsng/fantasy_world/particle/BlueflameParticle.java
@@ -0,0 +1,86 @@
+
+package siongsng.fantasy_world.particle;
+
+import siongsng.fantasy_world.SiongsngsFantasyWorldModElements;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.particles.ParticleType;
+import net.minecraft.particles.BasicParticleType;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.client.particle.SpriteTexturedParticle;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.IParticleRenderType;
+import net.minecraft.client.particle.IParticleFactory;
+import net.minecraft.client.particle.IAnimatedSprite;
+import net.minecraft.client.Minecraft;
+
+@SiongsngsFantasyWorldModElements.ModElement.Tag
+public class BlueflameParticle extends SiongsngsFantasyWorldModElements.ModElement {
+	public static final BasicParticleType particle = new BasicParticleType(false);
+	public BlueflameParticle(SiongsngsFantasyWorldModElements instance) {
+		super(instance, 22);
+		MinecraftForge.EVENT_BUS.register(this);
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+
+	@SubscribeEvent
+	public void registerParticleType(RegistryEvent.Register<ParticleType<?>> event) {
+		event.getRegistry().register(particle.setRegistryName("blueflame"));
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public void registerParticle(ParticleFactoryRegisterEvent event) {
+		Minecraft.getInstance().particles.registerFactory(particle, CustomParticleFactory::new);
+	}
+	@OnlyIn(Dist.CLIENT)
+	private static class CustomParticle extends SpriteTexturedParticle {
+		private final IAnimatedSprite spriteSet;
+		protected CustomParticle(ClientWorld world, double x, double y, double z, double vx, double vy, double vz, IAnimatedSprite spriteSet) {
+			super(world, x, y, z);
+			this.spriteSet = spriteSet;
+			this.setSize((float) 0.2, (float) 0.2);
+			this.particleScale *= (float) 1;
+			this.maxAge = (int) Math.max(1, 7 + (this.rand.nextInt(2) - 1));
+			this.particleGravity = (float) 0;
+			this.canCollide = true;
+			this.motionX = vx * 1;
+			this.motionY = vy * 1;
+			this.motionZ = vz * 1;
+			this.selectSpriteWithAge(spriteSet);
+		}
+
+		@Override
+		public IParticleRenderType getRenderType() {
+			return IParticleRenderType.PARTICLE_SHEET_OPAQUE;
+		}
+
+		@Override
+		public void tick() {
+			super.tick();
+			if (!this.isExpired) {
+				this.setSprite(this.spriteSet.get((this.age / 1) % 1 + 1, 1));
+			}
+		}
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	private static class CustomParticleFactory implements IParticleFactory<BasicParticleType> {
+		private final IAnimatedSprite spriteSet;
+		public CustomParticleFactory(IAnimatedSprite spriteSet) {
+			this.spriteSet = spriteSet;
+		}
+
+		public Particle makeParticle(BasicParticleType typeIn, ClientWorld worldIn, double x, double y, double z, double xSpeed, double ySpeed,
+				double zSpeed) {
+			return new CustomParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+		}
+	}
+}
