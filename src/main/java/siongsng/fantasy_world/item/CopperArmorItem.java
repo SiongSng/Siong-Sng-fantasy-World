72ba08b90e102cd614034d70fd62da153cc609b5
Barry23412 <rrt467778@gmail.com>
2021-01-22 13:54:05 +0800

    新增銅相關裝備(頭盔、胸甲、褲子、鞋子)

---
+++ src/main/java/siongsng/fantasy_world/item/CopperArmorItem.java
@@ -0,0 +1,104 @@
+
+package siongsng.fantasy_world.item;
+
+import siongsng.fantasy_world.SiongsngsFantasyWorldModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+@SiongsngsFantasyWorldModElements.ModElement.Tag
+public class CopperArmorItem extends SiongsngsFantasyWorldModElements.ModElement {
+	@ObjectHolder("siongsngs_fantasy_world:copper_armor_helmet")
+	public static final Item helmet = null;
+	@ObjectHolder("siongsngs_fantasy_world:copper_armor_chestplate")
+	public static final Item body = null;
+	@ObjectHolder("siongsngs_fantasy_world:copper_armor_leggings")
+	public static final Item legs = null;
+	@ObjectHolder("siongsngs_fantasy_world:copper_armor_boots")
+	public static final Item boots = null;
+	public CopperArmorItem(SiongsngsFantasyWorldModElements instance) {
+		super(instance, 116);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			@Override
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 23;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{3, 9, 8, 3}[slot.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return 14;
+			}
+
+			@Override
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(""));
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(CopperingotItem.block, (int) (1)));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return "copper_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 0f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "siongsngs_fantasy_world:textures/models/armor/copper_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("copper_armor_helmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "siongsngs_fantasy_world:textures/models/armor/copper_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("copper_armor_chestplate"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "siongsngs_fantasy_world:textures/models/armor/copper_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("copper_armor_leggings"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "siongsngs_fantasy_world:textures/models/armor/copper_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("copper_armor_boots"));
+	}
+}
