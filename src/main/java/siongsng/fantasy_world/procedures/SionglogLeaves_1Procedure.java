52e736c7afbf9b47e7cd1d4fa668d389c22dac25
Barry23412 <rrt467778@gmail.com>
2021-01-24 15:02:37 +0800

    修復再創造模式的時候破壞樹葉會掉落樹苗-感謝YT Mango回報此錯誤

--- src/main/java/siongsng/fantasy_world/procedures/SionglogLeaves_1Procedure.java
+++ src/main/java/siongsng/fantasy_world/procedures/SionglogLeaves_1Procedure.java
@@ -13,10 +13,16 @@ import net.minecraftforge.common.MinecraftForge;
 
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.GameType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.Minecraft;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -29,6 +35,11 @@ public class SionglogLeaves_1Procedure extends SiongsngsFantasyWorldModElements.
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				SiongsngsFantasyWorldMod.LOGGER.warn("Failed to load dependency entity for procedure SionglogLeaves_1!");
+			return;
+		}
 		if (dependencies.get("x") == null) {
 			if (!dependencies.containsKey("x"))
 				SiongsngsFantasyWorldMod.LOGGER.warn("Failed to load dependency x for procedure SionglogLeaves_1!");
@@ -49,25 +60,41 @@ public class SionglogLeaves_1Procedure extends SiongsngsFantasyWorldModElements.
 				SiongsngsFantasyWorldMod.LOGGER.warn("Failed to load dependency world for procedure SionglogLeaves_1!");
 			return;
 		}
+		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == Siong_logLeavesBlock.block.getDefaultState().getBlock())) {
-			if ((Math.random() < 0.4)) {
-				if (world instanceof World && !world.isRemote()) {
-					ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(SiongSaplingitemItem.block, (int) (1)));
-					entityToSpawn.setPickupDelay((int) 10);
-					world.addEntity(entityToSpawn);
+		if ((new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayerEntity) {
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.SURVIVAL;
+				} else if (_ent instanceof PlayerEntity && _ent.world.isRemote()) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((ClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.SURVIVAL;
+				}
+				return false;
+			}
+		}.checkGamemode(entity))) {
+			if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == Siong_logLeavesBlock.block.getDefaultState()
+					.getBlock())) {
+				if ((Math.random() < 0.4)) {
+					if (world instanceof World && !world.isRemote()) {
+						ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(SiongSaplingitemItem.block, (int) (1)));
+						entityToSpawn.setPickupDelay((int) 10);
+						world.addEntity(entityToSpawn);
+					}
 				}
 			}
-		}
-		if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == RedwoodLeavesBlock.block.getDefaultState().getBlock())) {
-			if ((Math.random() < 0.4)) {
-				if (world instanceof World && !world.isRemote()) {
-					ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(RedwoodsaplingitemItem.block, (int) (1)));
-					entityToSpawn.setPickupDelay((int) 10);
-					world.addEntity(entityToSpawn);
+			if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == RedwoodLeavesBlock.block.getDefaultState()
+					.getBlock())) {
+				if ((Math.random() < 0.4)) {
+					if (world instanceof World && !world.isRemote()) {
+						ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(RedwoodsaplingitemItem.block, (int) (1)));
+						entityToSpawn.setPickupDelay((int) 10);
+						world.addEntity(entityToSpawn);
+					}
 				}
 			}
 		}
